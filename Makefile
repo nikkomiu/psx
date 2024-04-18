bd6f816e8ec9c45a5151afa00bd727bab5b85c20
Nikko Miu <nikko@miu.guru>
2024-04-17 21:21:01 -0700

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+gpu:
+	$(MAKE) -C gpu
---
+++ gpu/main.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <libgte.h>
+#include <libetc.h>
+#include <libgpu.h>
+
+#define VIDEO_MODE 0
+#define SCREEN_RES_X 320
+#define SCREEN_RES_Y 240
+#define SCREEN_CENTER_X (SCREEN_RES_X >> 1)
+#define SCREEN_CENTER_Y (SCREEN_RES_Y >> 1)
+
+void ScreenInit(void)
+{
+
+}
+
+void DisplayFrame(void)
+{
+
+}
+
+void Setup(void)
+{
+
+}
+
+void Update(void)
+{
+
+}
+
+void Render(void)
+{
+
+}
+
+int main(void)
+{
+
+    return 0;
+}
